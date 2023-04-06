7e6cf09f2c39350db6a8883bf4c9a0946b9ce735
VirLg <19virlg@gmail.com>
2023-04-06 18:14:49 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log(5)
+// task-1
+// // Change code below this line
+// const productName =  "Droid";
+// const pricePerItem = 2000;
+// // console.log(productName);
+// // 'Droid'
+// // console.log(pricePerItem);
+// // 2000
+
+// task-2
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// productName="Repair droid";
+// pricePerItem=3500;
+// // Change code below this line
+
+//  task-3
+const topSpeed = 160;
+const distance= 617.54;
+const login= "mango935";
+const isOnline= true;
+const isAdmin= false;
+
+
+
+
