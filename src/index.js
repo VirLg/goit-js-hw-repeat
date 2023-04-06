cc6841c191d9f5538165fb1c18f7328c00f95eeb
VirLg <19virlg@gmail.com>
2023-04-06 18:37:47 +0300

    task7

--- src/index.js
+++ src/index.js
@@ -14,13 +14,34 @@
 // pricePerItem=3500;
 // // Change code below this line
 
-//  task-3
-const topSpeed = 160;
-const distance= 617.54;
-const login= "mango935";
-const isOnline= true;
-const isAdmin= false;
+// //  task-3
+// const topSpeed = 160;
+// const distance= 617.54;
+// const login= "mango935";
+// const isOnline= true;
+// const isAdmin= false;
 
+// //  task-4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// // Change code below this line
+// const totalPrice = pricePerItem*orderedQuantity;
+
+//  task-5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+// // Change code below this line
+
+//  task-6
+// Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice  = pricePerDroid*orderedQuantity+deliveryFee
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+
+//  task-7
 
 
 
